518ef3925c92a9480f0dc778eabdeabdc679e7c1
mhmdyouali <mhmd.you@yahoo.com>
2022-12-10 16:47:12 +0200

    project init #test

--- src/main/java/com/spring/BootPlayerApiApplication.java
+++ src/main/java/com/spring/BootPlayerApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BootPlayerApiApplication {
 
     public static void main(String[] args) {
+        //test
         SpringApplication.run(BootPlayerApiApplication.class, args);
     }
 
