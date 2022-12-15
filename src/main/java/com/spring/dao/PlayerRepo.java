098ee7b18ee141dc4f5ff4a1e1c99859fcdebb36
mhmdyouali <mhmd.you@yahoo.com>
2022-12-15 02:28:04 +0200

    JpaRepo custom functions

--- src/main/java/com/spring/dao/PlayerRepo.java
+++ src/main/java/com/spring/dao/PlayerRepo.java
@@ -2,6 +2,26 @@ package com.spring.dao;
 
 import com.spring.model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
 
 public interface PlayerRepo extends JpaRepository<Player,Integer> {
+    Player findByName(@RequestParam String name);
+
+    List<Player> findByNameContaining(@RequestParam String name);
+
 }
+
+// Get  ====> http://localhost:9090/restapi/players
+// Get  ====> http://localhost:9090/restapi/players/id
+// Post  ====> http://localhost:9090/restapi/players
+// Put  ====> http://localhost:9090/restapi/players
+// Delete  ====> http://localhost:9090/restapi/players/id
+// Get  ====> http://localhost:9090/restapi/players/search/findByName?name=mohamed
+
+// Get  ====> http://localhost:9090/restapi/players/search/findByNameContaining?name=mohamed
+
+
