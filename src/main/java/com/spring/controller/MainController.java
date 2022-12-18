bb1bc5cf6fb44218512cb8a78fc99101f06fbb5c
mhmdyouali <mhmd.you@yahoo.com>
2022-12-18 23:17:02 +0200

    get all players

--- src/main/java/com/spring/controller/MainController.java
+++ src/main/java/com/spring/controller/MainController.java
@@ -1,9 +1,15 @@
 package com.spring.controller;
 
+import com.spring.model.Player;
+import com.spring.service.PlayerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
+import java.util.List;
 
 
 // http://localhost:9090/
@@ -11,11 +17,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/main")
 //http://localhost:9090/main
 public class MainController {
+    @Autowired
+    private PlayerService playerService;
 
-    // http://localhost:9090/main/profile
-    @GetMapping("/profile")
-    public String test(Model model){
-        model.addAttribute("name","mohamed yosri");
+    // http://localhost:9090/main/players  ==> GET
+    @GetMapping("/players")
+    public String getAll(Model model){
+        List<Player> players = playerService.findAll();
+        model.addAttribute("players",players);
+        model.addAttribute("currentDate",new Date());
         return "home";
     }
+
+
 }
